0c1a38fc7c00dc3b4178c7a1d729e17749961294
Carlos Reynaga <carandreyn@gmail.com>
2022-10-10 11:23:37 -0700

    init

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Pomodoro
+{
+    partial class Pomodoro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pomodoro));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.CountdownLabel = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Image = ((System.Drawing.Image)(resources.GetObject("StartButton.Image")));
+            this.StartButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.StartButton.Location = new System.Drawing.Point(226, 237);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(113, 63);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton.Image = ((System.Drawing.Image)(resources.GetObject("ResetButton.Image")));
+            this.ResetButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ResetButton.Location = new System.Drawing.Point(436, 237);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(138, 63);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(354, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Session";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(195, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 41);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Session Length";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(408, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 41);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Break Length";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(226, 378);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(445, 378);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // CountdownLabel
+            // 
+            this.CountdownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountdownLabel.Location = new System.Drawing.Point(174, 67);
+            this.CountdownLabel.Name = "CountdownLabel";
+            this.CountdownLabel.Size = new System.Drawing.Size(453, 121);
+            this.CountdownLabel.TabIndex = 10;
+            this.CountdownLabel.Text = "00:00:00";
+            this.CountdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Pomodoro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CountdownLabel);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StartButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pomodoro";
+            this.Text = "Pomodoro";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label CountdownLabel;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pomodoro
+{
+    public partial class Pomodoro : Form
+    {
+        int seconds;
+        public Pomodoro()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            seconds = Convert.ToInt32(numericUpDown1.Value * 60);
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(seconds--);
+
+            string answer = string.Format("{0:D2}h : {1:D2}m : {2:D2}s",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds);
+            CountdownLabel.Text = answer.ToString();
+            if(seconds < 0)
+            {
+                timer.Stop();
+                numericUpDown1.Enabled = true;
+                numericUpDown2.Enabled = true;
+                seconds = Convert.ToInt32(numericUpDown2.Value * 60);
+                timer2.Start();
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            seconds = 0;
+            numericUpDown1.Value = 0;
+            CountdownLabel.Text = "00:00:00";
+            timer.Stop();
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+
+            TimeSpan t = TimeSpan.FromSeconds(seconds--);
+
+            string answer = string.Format("{0:D2}h : {1:D2}m : {2:D2}s",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds);
+
+            CountdownLabel.Text = answer.ToString();
+            if (seconds < 0)
+            {
+                timer2.Stop();
+                numericUpDown1.Enabled = true;
+                numericUpDown2.Enabled = true;
+            }
+        }
+    }
+}
